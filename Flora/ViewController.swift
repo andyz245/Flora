6442e5e3beec0fbcf0f04be775b25f31322d72ac
Andy Zhou <andyzhou1989@gmail.com>
2019-04-24 23:49:27 -0700

    Initial Commit

---
+++ Flora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flora
+//
+//  Created by Andy  Zhou on 4/24/19.
+//  Copyright © 2019 Andy  Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
