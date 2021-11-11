a9d2a39f7c9498018139e44a910669019d239174
andyz245 <andyzhou1989@gmail.com>
2021-11-10 18:33:24 -0600

    pushing app from local dir

--- Flora/ViewController.swift
+++ Flora/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        self.navigationItem.title = "Select Image!"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(self.imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //Finds the image the user picked, sets view, converts to ciimage for model to detect
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageView.image = image
+            //MLModel uses ciimages
+            guard let ciimage = CIImage(image: image) else {
+                fatalError("Could not convert image")
+            }
+            
+            instructionLabel.isHidden = true
+            detect(image: ciimage)
+        }
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Error loading model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Could not process image")
+            }
+            
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
 }
 
